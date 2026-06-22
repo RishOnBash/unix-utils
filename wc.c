328485e2e5a45f59efd8b6dbeb889455f4612eec
Rish <266709254+RishOnBash@users.noreply.github.com>
2026-06-22 11:04:27 +0530

    add word count option

--- wc.c
+++ wc.c
@@ -2,35 +2,32 @@
 #include <string.h>
 
 /*
- * wc clone, supports 2 options as of now
- * -c (character count) and -l (line count)
- * options are hardcoded so wc file won't work
+ * wc clone, options are hardcoded as of now 
+ * so ./wc <file> won't work, it need option
  * will fix that later, as i get better in C
  *
  * Author: Rish
- * Date: 16 June, 2026
+ * Date: 22 June, 2026
  * License: MIT
  */
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
-        printf ("Usage: %s [-c | -l] <file>\n", argv[0]);
+        printf ("Usage: %s [ -c | -l | -w ] <file>\n", argv[0]);
         return 1;
     }
 
     FILE *fp = fopen(argv[2], "r");     //argv[2] = file
     if (!fp) {
-        printf ("%s: no such file\n", argv[2]);
-        return 1;
+        printf ("%s: no such file\n", argv[2]); return 1;
     }
 
     // get character count
     if (strcmp(argv[1], "-c") == 0) {   //argv[1] = flag
         int c, count = 0;
-        while ((c = fgetc(fp)) != EOF) {
+        while ((c = fgetc(fp)) != EOF) 
             count++;
-        }
-        printf ("Total characters: %i\n", count);
+        printf ("%i %s\n", count, argv[2]);
     }
         
     // get line count
@@ -41,9 +38,21 @@ int main(int argc, char *argv[]) {
             if (strchr(buffer, '\n'))
                 count ++;
         }
-        printf ("Total lines: %i\n", count);
-    } else {
-        printf ("Usage: %s [ -c | -l ] <file>\n", argv[0]);
+        printf ("%i %s\n", count, argv[2]);
+    } 
+
+    // get word count
+    else if (strcmp(argv[1], "-w") == 0) {
+        char buffer [255];  // how big a word can go, idk!?
+        int c, count = 0;
+        // no bounded check, it returns incorrect word count
+        while ((c = fscanf(fp, "%s", buffer)) != EOF)
+            count++;
+        printf ("%i %s\n", count, argv[2]);
+    }
+    
+    else {
+        printf ("Usage: %s [ -c | -l | -w ] <file>\n", argv[0]);
         return 1;
     }
     
