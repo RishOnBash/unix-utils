f51aab05d71ff6beebe1a63f463bc93b8bfe9f3e
Rish <266709254+RishOnBash@users.noreply.github.com>
2026-06-23 15:25:55 +0530

    better error message and multiple file reading

--- cat.c
+++ cat.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
+#include <errno.h>
 
 /* 
  * display file contents to STDOUT
  *
- * Author: Rish
- * Date: 15 June, 2026
+ * Author: Rish <RishOnBash>
+ * Date: 23 June, 2026
  * License: MIT
  */
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf ("Usage: %s <file>\n", argv[0]);
-        return 1;
-    }
-    FILE *fp = fopen(argv[1], "r");
-    if (!fp) {
-        printf ("%s: %s: No such file\n", argv[0],argv[1]);
-        return 1;
-    }
-    char buffer [255];
-    while (fgets(buffer, sizeof(buffer), fp)) {
-        printf ("%s", buffer);
-    }
+    if (argc < 2)
+        fprintf (stderr, "Usage: %s FILE ...\n", argv[0]);
+
+    for (int i = 1; i < argc; i++) {
+        FILE *fp = fopen(argv[i], "r");
+        if (!fp) {
+        fprintf (stderr, "%s: %s: %s\n", argv[0],argv[i], strerror(errno));
+        continue;
+        }
+        char buffer [1024];
+        while (fgets(buffer, sizeof(buffer), fp)) {
+            printf ("%s", buffer);
+        }
     fclose(fp);
+    }
     return 0;
 }
