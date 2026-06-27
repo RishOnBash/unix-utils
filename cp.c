4a7dd94d7888fb35756ce4a12b8a0c6498d923b5
Rish <266709254+RishOnBash@users.noreply.github.com>
2026-06-27 11:29:39 +0530

    use binary-based copying

--- cp.c
+++ cp.c
@@ -1,10 +1,11 @@
+#include <errno.h>
 #include <stdio.h>
 
 /*
  * copy files from source to destination
  *
  * Author: Rish
- * Date: 15 June, 2026
+ * Date: 27 June, 2026
  * License: MIT
  */
 
@@ -13,19 +14,25 @@ int main(int argc, char *argv[]) {
         printf ("Usage: %s <source_file> <destination_file>\n", argv[0]);
         return 1;
     }
-    FILE *fps = fopen(argv[1], "r");    // fps = source file
-    FILE *fpd = fopen(argv[2], "w");    // fpd = destination file
+    FILE *fps = fopen(argv[1], "rb");    // fps = source file
+    FILE *fpd = fopen(argv[2], "wb");    // fpd = destination file
 
-    if (!fps || !fpd) {
-        puts ("Error: No such file(s)");
-        return 1;
+    if (!fps) {
+        fprintf (stderr, "%s: source '%s': %s\n", argv[0], argv[1], strerror(errno));
+        return 11;  // source error
     }
 
-    char buffer [1024];
-    while (fgets(buffer, sizeof(buffer), fps)) {
-        fprintf (fpd, "%s", buffer);
+    if (!fpd) {
+        fprintf (stderr, "%s: destination '%s': %s\n", argv[0], argv[2], strerror(errno));
+        fclose(fps);
+        return 22;  // destination error
     }
-    // close files 
+
+    unsigned char buffer [4096];
+    size_t _read;
+    while ((_read = fread(buffer, 1, sizeof(buffer), fps)) > 0)
+        fwrite(buffer, 1, _read, fpd);
+        
     fclose(fps);
     fclose(fpd);
 
