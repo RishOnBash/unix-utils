d7d617037261c2c62197962aee4463d4c21c8fd1
Rish <266709254+RishOnBash@users.noreply.github.com>
2026-06-27 13:00:49 +0530

    stdout using fread() and fwrite()

--- cat.c
+++ cat.c
@@ -5,7 +5,7 @@
  * display file contents to STDOUT
  *
  * Author: Rish <RishOnBash>
- * Date: 23 June, 2026
+ * Date: 27 June, 2026
  * License: MIT
  */
 
@@ -14,16 +14,19 @@ int main(int argc, char *argv[]) {
         fprintf (stderr, "Usage: %s FILE ...\n", argv[0]);
 
     for (int i = 1; i < argc; i++) {
-        FILE *fp = fopen(argv[i], "r");
+        FILE *fp = fopen(argv[i], "rb");
         if (!fp) {
         fprintf (stderr, "%s: %s: %s\n", argv[0],argv[i], strerror(errno));
         continue;
         }
-        char buffer [1024];
-        while (fgets(buffer, sizeof(buffer), fp)) {
-            printf ("%s", buffer);
-        }
+
+        unsigned char buffer [4096];
+        size_t b_read;   // bytes read
+        while ((b_read = fread(buffer, 1, sizeof(buffer), fp)) > 0)
+            fwrite(buffer, 1, b_read, stdout);
+
     fclose(fp);
     }
+
     return 0;
 }
