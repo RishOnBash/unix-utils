acd1896ed43ee48ed7d9e945456f212a18630480
Rish <266709254+RishOnBash@users.noreply.github.com>
2026-06-29 13:39:37 +0530

    refactor: use POSIX read/write with stdin support

--- cat.c
+++ cat.c
@@ -1,32 +1,37 @@
-#include <stdio.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 /* 
  * display file contents to STDOUT
  *
  * Author: Rish <RishOnBash>
- * Date: 27 June, 2026
+ * Date: 29 June, 2026
  * License: MIT
  */
 
 int main(int argc, char *argv[]) {
-    if (argc < 2)
-        fprintf (stderr, "Usage: %s FILE ...\n", argv[0]);
+    ssize_t b_read;     //bytes read
+    unsigned char buf[BUFSIZ];
 
-    for (int i = 1; i < argc; i++) {
-        FILE *fp = fopen(argv[i], "rb");
-        if (!fp) {
-        fprintf (stderr, "%s: %s: %s\n", argv[0],argv[i], strerror(errno));
-        continue;
+    if (argc > 1) {
+        for (int i=1; i<argc; i++) {
+            int fd = open(argv[i], O_RDONLY);
+            if (fd < 0) {
+                fprintf (stderr, "%s: %s: %s\n",
+                        argv[0], argv[i], strerror(errno));
+                continue;
+            }
+            while ((b_read = read(fd, buf, BUFSIZ)) > 0)
+                write(1, buf, b_read);
+            close(fd);
         }
 
-        unsigned char buffer [4096];
-        size_t b_read;   // bytes read
-        while ((b_read = fread(buffer, 1, sizeof(buffer), fp)) > 0)
-            fwrite(buffer, 1, b_read, stdout);
-
-    fclose(fp);
-    }
-
+    // read from STDIN
+    } else
+        while ((b_read = read(0, buf, BUFSIZ)) > 0)
+            write(1, buf, b_read);
     return 0;
 }
