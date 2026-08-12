0be24951efea90ad89d6e2ece418ead5953c762f
Rish <266709254+RishOnBash@users.noreply.github.com>
2026-08-12 13:43:24 +0530

    add mkdir.c

---
+++ mkdir.c
@@ -0,0 +1,29 @@
+/*
+ * mkdir clone - make directories
+ *
+ * Author: Rish <RishOnBash>
+ * Date: August 12, 2026
+ * License: MIT
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        dprintf(2, "%s: missing operand\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 1; i < argc; i++) {
+        if (mkdir(argv[i], 0700) < 0) {
+            dprintf(2, "%s: %s: %s\n",
+                    argv[0], argv[i], strerror(errno));
+            continue;
+        }
+    }
+    return 0;
+}
